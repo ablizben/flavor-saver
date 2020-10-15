7fc844234218dfcac3bf8030ba04fe7babff9ef7
Thomas Coradetti <tmcoradetti@gmail.com>
2020-10-14 20:01:12 -0400

    adding functional firebase login/signup

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import ForgotPassword from './components/ForgotPassword.js';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <AuthProvider>
-      <Container 
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
+    <Container 
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div 
+        className="w-100" 
+        style={{ maxWidth: "400px" }}
       >
-        <div 
-          className="w-100" 
-          style={{ maxWidth: "400px" }}
-        >
-          <Signup />
-        </div>
-        
-      </Container>  
-    </AuthProvider>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>  
+        </Router>
+      </div>
+    </Container>  
+    
     
   );
 }
--- reactpractice/src/contexts/AuthContext.js
+++ reactpractice/src/contexts/AuthContext.js
@@ -16,6 +16,18 @@ export function AuthProvider({ children }) {
         return auth.createUserWithEmailAndPassword(email, password);
     };
 
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password);
+    };
+
+    function logout() {
+        return auth.signOut();
+    };
+
+    function resetPassword(email) {
+        return auth.sendPasswordResetEmail(email);
+    };
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user);
@@ -28,7 +40,10 @@ export function AuthProvider({ children }) {
 
     const value = {
         currentUser,
-        signup
+        login,
+        signup,
+        logout,
+        resetPassword
     };
 
     return (
