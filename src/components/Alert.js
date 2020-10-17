a81434c4260a26e21b2e2ecc82f44c793426218a
siminski <laurensiminski@gmail.com>
2020-10-17 16:43:51 -0400

    alert component created

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "../App.css";
 
-const Alert = (props) => (
-    <div>
-        
-    </div>
-);
+function Alert(props) {
+    return (
+        <div class={`alert alert-${props.type} alert-dismissible fade show`} role="alert">
+            <span class="alert-copy">{props.children}</span>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true"><i class="fal fa-times"></i></span></button>
+        </div>
+    );
+}
 
 export default Alert;
-
-
