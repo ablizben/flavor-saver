bd1f13242820499096eab9aaf15679400f5613fa
siminski <laurensiminski@gmail.com>
2020-10-17 14:42:13 -0400

    profile info component created

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,12 +1,49 @@
-import React from "react";
-import "../App.css";
+import React from 'react';
 
-const ProfileInfo = (props) => (
-    <div>
-        
-    </div>
-);
+function styles(user) {
+	const {image} = user;
+	return {
+		background: `url(${image})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center center'
+	}
+}
+
+function ProfileInfo(props) {
+    return (
+        <div className="profile-info">
+            <div className="profile-image" style={styles(props)}></div>
+            <h1 className="mb-1">{props.name}</h1>
+            <a href={props.locationURL} className="location text-center d-block"><i className="fas fa-map-marker-alt"></i> {props.location}</a>
+            <div className="stats row m-0 text-center">
+                <div className="col-4">
+                    <span className="num">{props.posts}</span>
+                    Posts
+                </div>
+                <div className="col-4">
+                    <span className="num">{props.followers}</span>
+                    Followers
+                </div>
+                <div className="col-4">
+                    <span className="num">{props.following}</span>
+                    Following
+                </div>
+            </div>
+            <div className="col">
+                <a href="edit-profile.html" className="btn btn-primary w-100">Edit Profile</a>
+            </div>
+            <div className="col">
+                <p className="bio text-center">{props.bio}</p>
+                <a href={props.website} className="location text-center d-block"><i className="fas fa-link"></i> {props.website}</a>
+            </div>
+            <div className="col text-center">
+                <div className="btn-group text-center">
+                    <a href="#" className="btn btn-outline btn-small">Reservations</a>
+                    <a href="#" className="btn btn-outline btn-small">Order Online</a>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default ProfileInfo;
-
-
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -25,6 +25,19 @@ const gridPosts = [
 class Profile extends Component {
     render() {
         return (
+			<ProfileInfo 
+				image="https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+				name="Lauren Siminski" 
+				location="Baltimore, MD"
+				locationURL="#"
+				posts="20"
+				followers="20"
+				following="20"
+				bio="Patio dining Wednesday - Sunday."
+				website="https://www.woodberrykitchen.com"
+				reservations="#"
+				orderOnline="#"
+			/>
             <Grid posts={gridPosts} />
         );
     }
